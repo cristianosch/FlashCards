d46b987c9aac11c2f6c39f79ea1be1b9c0fcf0ab
Cristiano Schroeder <webdesignercristiano@gmail.com>
2024-01-29 19:12:14 +0000

    mudança de database

--- study_async/settings.py
+++ study_async/settings.py
@@ -79,10 +79,18 @@ WSGI_APPLICATION = 'study_async.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# settings.py
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
