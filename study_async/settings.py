7a1a5f992790433f84ff53819e02e2b04b264313
Cristiano Schroeder <webdesignercristiano@gmail.com>
2024-01-29 21:32:51 +0000

    adicionado static_root

--- study_async/settings.py
+++ study_async/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'flashcard/static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR)
 MEDIA_URL = '/media/'
-
+STATIC_ROOT = '/flashcard/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
