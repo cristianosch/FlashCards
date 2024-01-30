083590680e7bbb8b760429b8fb8bc733967dc83f
Cristiano Schroeder <webdesignercristiano@gmail.com>
2024-01-30 14:43:56 +0000

    STATIC_ROOT acidionado nova path

--- study_async/settings.py
+++ study_async/settings.py
@@ -134,10 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'flashcard/static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR)
 MEDIA_URL = '/media/'
+
 #STATIC_ROOT = '/home/flashcards.uk.to/public_html/study_async/flashcard/static/'
 
 # Default primary key field type
