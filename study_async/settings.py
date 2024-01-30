d647fd9e761382ea60875477c8ca719db342509b
Cristiano Schroeder <webdesignercristiano@gmail.com>
2024-01-30 14:57:12 +0000

    root

--- study_async/settings.py
+++ study_async/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/flashcards.uk.to/public_html/study_async/flashcard/static'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'flashcard/static'),)
