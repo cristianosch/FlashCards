607df534fe6f8024595a5e621872f84615fabdfa
Cristiano Schroeder <webdesignercristiano@gmail.com>
2024-01-30 18:59:02 +0000

    added witenoise

--- study_async/settings.py
+++ study_async/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -142,6 +144,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR)
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = '/home/flashcards.uk.to/public_html/study_async/static'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
